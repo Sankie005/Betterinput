c67a12c13ec10a4042ec04762453ac34ffcc0302
Sankie005 <89256230+Sankie005@users.noreply.github.com>
2023-05-15 08:59:44 +0530

    Updated to version 0.5.1
    added a "mixed" type for lists allowing you to enter any variable

--- Inputbetter/functions.py
+++ Inputbetter/functions.py
@@ -1,17 +1,27 @@
 def btrinpt(type,k,n):
     global mylist
     mylist=[]
-    if type=="str":
-        for i in range(0,n):
-            ic=str(i)
-            kf=str(k+ic+":")
-            xu=str(input(kf))
-            mylist.append(xu)
-        return mylist    
-    if type=="int":
-        for j in range(0,n):
-            jc=str(j)
-            kf=str(k+jc+":")
-            xu=int(input(kf))
-            mylist.append(xu)
-        return mylist
+    try:
+        if type=="str":
+            for i in range(0,n):
+               ic=str(i)
+               kf=str(k+ic+":")
+               xu=str(input(kf))
+               mylist.append(xu)
+               return mylist    
+        elif type=="int":
+            for j in range(0,n):
+             jc=str(j)
+             kf=str(k+jc+":")
+             xu=int(input(kf))
+             mylist.append(xu)
+             return mylist
+        elif type=="mixed":
+           for l in range(0,l):
+              lc=str(l)
+              kf=str(k+ic+":")
+              xu=input(kf)
+              mylist.append(xu)
+              return mylist
+    except: 
+       print("Invalid type entered")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='Inputbetter',
     packages=find_packages(include=['Inputbetter']),
-    version='0.5.0',
+    version='0.5.1',
     description='A library to make entering data into lists easier!',
     author='Sankie',
     license='MIT'
